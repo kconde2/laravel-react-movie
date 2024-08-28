e1ab272378f6867812b20f7f1eb49aabe6f42756
kconde2 <43377621+kconde2@users.noreply.github.com>
2024-08-28 16:24:14 +0200

    feat(movies): use api data to update (#9)

--- app/Http/Controllers/Movies/UpdateMovieController.php
+++ app/Http/Controllers/Movies/UpdateMovieController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Movies;
 use App\Http\Controllers\Controller;
 use Domain\Movies\Actions\UpdateMovieAction;
 use Domain\Movies\Data\MovieData;
+use Domain\Movies\Interfaces\MovieApiDataProviderInterface;
 use Domain\Movies\Models\Movie;
 use Illuminate\Http\Request;
 
@@ -13,9 +14,15 @@ class UpdateMovieController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request, UpdateMovieAction $updateMovieAction, int $movieId)
-    {
+    public function __invoke(
+        Request $request,
+        UpdateMovieAction $updateMovieAction,
+        MovieApiDataProviderInterface $movieApiDataProvider,
+        int $movieId
+    ) {
         $movie = Movie::query()->findOrFail($movieId);
+
+        $movieDetails = $movieApiDataProvider->getMovieDetails($movie->tmdb_id);
 
         $validated = $request->validate([
             'title' => 'required|string|max:255',
@@ -23,10 +30,14 @@ class UpdateMovieController extends Controller
         ]);
 
         $updateMovieAction->handle($movie, MovieData::from(
-            array_merge($movie->toArray(), $validated, [
-                'adult' => $movie->is_adult,
-                'video' => $movie->is_video,
-            ])
+            array_merge($movie->toArray(),
+                $movieDetails,
+                $validated,
+                [
+                    'adult' => $movie->is_adult,
+                    'video' => $movie->is_video,
+                ]
+            )
         ));
 
         return redirect()
