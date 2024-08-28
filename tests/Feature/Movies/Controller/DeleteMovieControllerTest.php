e185f16430170a4a8fe5df7e7f93358fece5e0a3
Kaba CONDE <kabaconde15@gmail.com>
2024-08-28 13:36:01 +0200

    feat(movies): add delete button

--- tests/Feature/Movies/Controller/DeleteMovieControllerTest.php
+++ tests/Feature/Movies/Controller/DeleteMovieControllerTest.php
@@ -5,18 +5,22 @@ use Domain\Movies\Models\Movie;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 use function Pest\Laravel\actingAs;
+use function Pest\Laravel\delete;
 
 uses(RefreshDatabase::class);
 
-it('deletes a movie successfully', function () {
-    // Arrange
+beforeEach(function () {
     /** @var Illuminate\Contracts\Auth\Authenticatable */
     $user = User::factory()->create();
     actingAs($user);
+});
+
+it('deletes a movie successfully', function () {
+    // Arrange
     $movie = Movie::factory()->create();
 
     // Act
-    $response = actingAs($user)->delete("/dashboard/movies/trending/delete/$movie->id");
+    $response = delete("/dashboard/movies/trending/delete/{$movie->id}");
 
     // Assert
     $response->assertStatus(302);
@@ -25,13 +29,8 @@ it('deletes a movie successfully', function () {
 });
 
 it('returns a 404 for an incorrect URL', function () {
-    // Arrange
-    /** @var Illuminate\Contracts\Auth\Authenticatable */
-    $user = User::factory()->create();
-    actingAs($user);
-
     // Act
-    $response = actingAs($user)->delete('/dashboard/movies/trending/incorrect-url');
+    $response = delete('/dashboard/movies/trending/incorrect-url');
 
     // Assert
     $response->assertStatus(404);
@@ -39,16 +38,20 @@ it('returns a 404 for an incorrect URL', function () {
 
 it('returns a 404 when trying to delete a non-existent movie', function () {
     // Arrange
-    /** @var Illuminate\Contracts\Auth\Authenticatable */
-    $user = User::factory()->create();
-    actingAs($user);
-
-    // Utilisez un ID de film qui n'existe pas (par exemple, 9999)
     $nonExistentMovieId = 9999;
 
     // Act
-    $response = actingAs($user)->delete("/dashboard/movies/trending/delete/$nonExistentMovieId");
+    $response = delete("/dashboard/movies/trending/delete/{$nonExistentMovieId}");
 
     // Assert
     $response->assertStatus(404);
 });
+
+it('redirects back unauthenticateds users when trying to delete a movie', function () {
+    $movie = Movie::factory()->create();
+
+    $response = delete("/dashboard/movies/trending/delete/{$movie->id}");
+
+    $response->assertRedirect('/');
+    $response->assertStatus(302);
+}, ['skip_before' => true]);
